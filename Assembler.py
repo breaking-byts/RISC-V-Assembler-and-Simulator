4af0bfcb1749c76fd5d0179712870632bb680a69
Anshu17anand <anshuman24089@iiitd.ac.in>
2025-02-15 00:10:17 +0530

    a

--- Assembler.py
+++ Assembler.py
@@ -275,5 +275,27 @@ class Assembler:
             binary = f"{func7}{rs2}{rs1}{func3}{rd}{opcode}"
             return binary
 
+    def S_type(self, instruction):
+        if len(instruction) != 3:
+            raise SyntaxError(f"Invalid number of arguments for S-type instruction: {instruction}")
+        opcode = self.opcodes[instruction[0]]
+        rs2 = self.reg_to_binary(instruction[1])
+
+        mem_addr = instruction[2]
+        if '(' not in mem_addr or ')' not in mem_addr:
+            raise SyntaxError(f"Invalid memory address format: {mem_addr}")
+
+        offset = mem_addr[:mem_addr.find('(')]
+        rs1 = mem_addr[mem_addr.find('(') + 1:mem_addr.find(')')]
+        rs1 = self.reg_to_binary(rs1)
+        imm = self.get_immediate_binary(offset if offset else '0', 12)
+
+        imm1 = imm[:7]
+        imm2 = imm[7:]
+
+        func3 = self.func3[instruction[0]]
+        binary = f"{imm1}{rs2}{rs1}{func3}{imm2}{opcode}"
+        return binary
+
 
 
